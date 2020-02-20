fa2e7a701ef19645eb308ef5dbf0e1093237c5a7
Gutyerrez <srgutyerrez@gmail.com>
2020-02-20 10:54:20 -0300

    Create PlayerDeathListener.java

---
+++ src/main/java/com/redefocus/login/listeners/PlayerDeathListener.java
@@ -0,0 +1,10 @@
+package com.redefocus.login.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class PlayerDeathListener implements Listener {
+}
