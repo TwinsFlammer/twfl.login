b2480be630ed74d5b898dd3544773e0302b4255a
Gutyerrez <srgutyerrez@gmail.com>
2020-02-20 10:56:56 -0300

    Create PlayerBlockPlaceListener.java

---
+++ src/main/java/com/redefocus/login/listeners/PlayerBlockPlaceListener.java
@@ -0,0 +1,25 @@
+package com.redefocus.login.listeners;
+
+import com.redefocus.common.shared.permissions.group.GroupNames;
+import com.redefocus.common.shared.permissions.user.data.User;
+import com.redefocus.common.shared.permissions.user.manager.UserManager;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class PlayerBlockPlaceListener implements Listener {
+    @EventHandler
+    public void onPlace(BlockPlaceEvent event) {
+        Player player = event.getPlayer();
+
+        User user = UserManager.getUser(player.getUniqueId());
+
+        if (user.hasGroup(GroupNames.MANAGER)) return;
+
+        event.setCancelled(true);
+    }
+}
