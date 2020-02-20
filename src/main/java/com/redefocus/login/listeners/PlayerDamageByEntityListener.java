8e59fa2801e51bb27d08fb65f2be292dfa0e09d8
Gutyerrez <srgutyerrez@gmail.com>
2020-02-20 10:56:41 -0300

    Create PlayerDamageByEntityListener.java

---
+++ src/main/java/com/redefocus/login/listeners/PlayerDamageByEntityListener.java
@@ -0,0 +1,21 @@
+package com.redefocus.login.listeners;
+
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+/**
+ * Created by @SrGutyerrez
+ */
+public class PlayerDamageByEntityListener implements Listener {
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onDamage(EntityDamageByEntityEvent event) {
+        Entity entity = event.getEntity();
+        Entity damager = event.getDamager();
+
+        if (entity instanceof Player && damager instanceof Player) event.setCancelled(true);
+    }
+}
