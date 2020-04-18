4204f7737767e064767379b6984395ec190c2378
Gutyerrez <srgutyerrez@gmail.com>
2020-04-17 21:06:59 -0300

    Update BlockPhysicsListener.java

--- src/main/java/com/redecommunity/login/listeners/BlockPhysicsListener.java
+++ src/main/java/com/redecommunity/login/listeners/BlockPhysicsListener.java
@@ -1,31 +1,15 @@
 package com.redecommunity.login.listeners;
 
-import org.bukkit.Material;
-import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPhysicsEvent;
-
-import java.util.Arrays;
 
 /**
  * Created by @SrGutyerrez
  */
 public class BlockPhysicsListener implements Listener {
-    private final Material[] materials = new Material[]{
-            Material.SAPLING,
-            Material.SAND,
-            Material.ANVIL,
-            Material.GRAVEL
-    };
-
     @EventHandler
     public void onBlockPhysics(BlockPhysicsEvent event) {
-        Block block = event.getBlock();
-
-        Material material = block.getType();
-
-        if (Arrays.asList(materials).contains(material))
-            event.setCancelled(true);
+        event.setCancelled(true);
     }
 }
